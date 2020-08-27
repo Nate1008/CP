e8591b8079a2a43799935c60ee8989cce208e033
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-27 19:40:50 -0400

    CLEANED 331C

--- Problem331C.cpp
+++ Problem331C.cpp
@@ -28,6 +28,22 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    int c = 0;
+    while(n != 0) {
+        int a = n;
+        int d = log10(n);
+        int m = a % 10;
+        for(int i = 0; i < d; i++) {
+            a /= 10;
+            int t = a % 10;
+            m = max(m, t);
+        }
+        n -= m;
+        c++;
+    }
+    cout<<c;
     return 0;
 }
 
