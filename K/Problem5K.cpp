ec897f807c70087ec22f0ccbbbd4631cbdeeb457
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-27 15:48:09 -0400

    FINISHED AND SORTED 5K

---
+++ K/Problem5K.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <vector>
+#include <set>
+#include <bitset>
+#include <map>
+#include <deque>
+#include <string>
+
+#include <algorithm>
+#include <numeric>
+
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cmath>
+
+#define ll long long
+#define pi pair<int, int>
+#define f first
+#define s second
+#define vt vector
+
+using namespace std;
+
+int main() {
+    cout<<"Git Test";
+    return 0;
+}
+
