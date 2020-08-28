e1b7997e7ca663f9a069fa26d17b3899436acae9
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-28 15:24:29 -0400

    CLEANED 159B

--- Problem159B.cpp
+++ Problem159B.cpp
@@ -28,6 +28,31 @@
 using namespace std;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
+    map<int, int> caps;
+    map<int, int> markers;
+    map<pi, int> same;
+    int a = 0;
+    int b = 0;
+    for(int i = 0; i < n; i++) {
+        pi t;
+        cin >> t.f >> t.s;
+        markers[t.s]++;
+        same[t]++;
+    }
+
+    for(int i = 0; i < m; i++) {
+        pi t;
+        cin >> t.f >> t.s;
+        caps[t.s]++;
+        if(same[t] == 1) {
+            same[t]++;
+            b++;
+        }
+        a += min(caps[t.s], markers[t.s]);
+    }
+    cout<<a<<" "<<b;
     return 0;
 }
 
