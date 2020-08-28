818a18a55d1a41ead4f3e378cba23f59af2b33df
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-28 15:30:45 -0400

    CLEANED 159B

--- Problem159B.cpp
+++ Problem159B.cpp
@@ -56,6 +56,7 @@ int main() {
     for(int i = 0; i < past.size(); i++) {
         pi t = past[i];
         a += min(caps[t.s], markers[t.s]);
+        cout<<a<<"\t"<<t.s<<"\n";
     }
     cout<<a<<" "<<b;
     return 0;
