c5488248e7641b78d338ed82314353bac183a304
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-29 18:15:25 -0400

    FINISHED AND SORTED 169B

--- B/Problem169B.cpp
+++ B/Problem169B.cpp
@@ -32,7 +32,6 @@ int main() {
     string a, b;
     cin >> a >> b;
     sort(b.rbegin(), b.rend());
-    cout<<b<<"\n";
     int k = 0;
     for(int i = 0; i < a.length(); i++) {
         if(a[i] < b[k]){
