fc62ef09df018f6843f837e2b1b29ef22e0261ea
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-29 17:59:38 -0400

    CLEANED 169B

--- Problem169B.cpp
+++ Problem169B.cpp
@@ -32,10 +32,10 @@ int main() {
     string a, b;
     cin >> a >> b;
     sort(b.begin(), b.end());
-    int k = b.length();
+    int k = 0;
     for(int i = 0; i < a.length(); i++) {
         if(a[i] < b[k]){
-            a[i] = b[k--];
+            a[i] = b[k++];
             b[i] = '0';
         }
     }
