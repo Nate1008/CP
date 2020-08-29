d638142355425dd9bc0e08de687a142499551b92
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-29 11:19:23 -0400

    CLEANED 635A2

--- Problem635A.cpp
+++ Problem635A.cpp
@@ -49,7 +49,7 @@ int main() {
                         pi t = pi(j, i);
                         pi e = pi(x, y);
                         pi p = vio[v];
-                        if((p.f >= t.f && p.s <= t.s) && (p.f <= e.f && p.s <= e.s)) {
+                        if((p.f >= t.f && p.s >= t.s) && (p.f <= e.f && p.s <= e.s)) {
                             std::cout<<"POINT: "<<p.f<<" "<<p.s<<"\n";
                             pin++;
                         }
