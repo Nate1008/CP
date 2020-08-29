26ab6a77438733f6a64f4d21c77490a3d18dafa7
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-29 17:48:50 -0400

    CLEANED 169B

--- Problem169B.cpp
+++ Problem169B.cpp
@@ -31,7 +31,7 @@ using namespace std;
 int main() {
     string a, b;
     cin >> a >> b;
-    for(int i = 0; i < b.length(); i++) {
+    for(int i = 0; i < a.length(); i++) {
         if(a[i] == 9) {
             continue;
         }
