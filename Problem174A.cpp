2db7f551b415943ebf2ea70516cbdc16ffe7cd9e
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-30 18:24:51 -0400

    RAN 174A

--- Problem174A.cpp
+++ Problem174A.cpp
@@ -45,11 +45,13 @@ int main() {
         cin >> t;
         k = max(k, t);
     }
+    cout<<"K: "<<k<<"\n";
     for(int i = 0; i < n; i++) {
         float diff = k - vol[i];
         b -= diff;
         ans[i] = diff;
     }
+    cout<<"B: "<<b<<"\n";
     if(b <= 0) {
         cout<<-1;
         return 0;
