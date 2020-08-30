383e60088b9fb52e9abfe600c5d09c837a47c5b6
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-30 15:07:58 -0400

    CLEANED 1231C

--- Problem1231C.cpp
+++ Problem1231C.cpp
@@ -47,6 +47,7 @@ int main() {
                 mat[i][k] = min(mat[i+1][k]-1, mat[i][k+1]-1);
                 int diffv = mat[i+1][k] - mat[i-1][k];
                 int diffh = mat[i][k+1] - mat[i][k-1];
+                cout<<diffh<<"\t"<<diffv<<"\n";
                 if(diffv <= 1 || diffh <= 1) {
                     cout<<-1;
                     return 0;
