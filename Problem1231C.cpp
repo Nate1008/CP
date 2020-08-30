a62656efe04e76dc40735769cd3d544bb5b64167
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-30 15:20:08 -0400

    CLEANED 1231C

--- Problem1231C.cpp
+++ Problem1231C.cpp
@@ -51,7 +51,7 @@ int main() {
 
     for(int i = n-1; i > 0; i--) {
         for(int k = m-1; k > 0; k--) {
-            cout<<i<<"\t"<<k;
+            cout<<i<<"\t"<<k<<"\n";
             //cout<<;
             if(mat[i][k] == 0) {
                 mat[i][k] = min(mat[i+1][k]-1, mat[i][k+1]-1);
