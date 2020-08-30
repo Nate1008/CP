c3c4177bada29ddba92d0ee9754d01a68842ef57
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-30 17:49:59 -0400

    RAN 174A

--- Problem174A.cpp
+++ Problem174A.cpp
@@ -32,6 +32,27 @@
 using namespace std;
 
 int main() {
+    int n, b;
+    cin >> n >> b;
+    vi vol(n);
+    vi ans(n);
+    for(int i = 0; i < n; i++) {
+        cin >> vol[i];
+    }
+    sort(vol.rbegin(), vol.rend());
+    for(int i = 0; i < n; i++) {
+        double diff = vol[0] - vol[i];
+        b -= diff;
+        ans[i] = diff;
+    }
+    if(b < 0) {
+        cout<<-1;
+        return 0;
+    }
+    double div = b/n;
+    for(int i = 0; i < n; i++) {
+        cout<<ans[i]+div<<"\n";
+    }
     return 0;
 }
 
