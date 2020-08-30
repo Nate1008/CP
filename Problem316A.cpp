fb4060454fadd7da64ce67fd98e61ad3e0ad21b7
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-30 09:59:49 -0400

    CREATED 316A

---
+++ Problem316A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <vector>
+#include <set>
+#include <bitset>
+#include <map>
+#include <deque>
+#include <string>
+
+#include <algorithm>
+#include <numeric>
+
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cmath>
+#include <ctype>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define f first
+#define s second
+#define vt vector
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
