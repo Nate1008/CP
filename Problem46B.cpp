0fcb188f421bcc519f2cb7e5606f193a23dda439
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-29 20:30:19 -0400

    RAN 46B

--- Problem46B.cpp
+++ Problem46B.cpp
@@ -58,7 +58,6 @@ int main() {
 
         int maxi = 0;
         for(int b = 0; b < dist.size(); b++) {
-            cout<<dist[b]<<"\n";
             if(dist[b] > dist[maxi]) {
                 maxi = b;
             }
