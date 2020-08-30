3029b439dc8c0bf422e04c0ac23ae705409e7b9a
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-30 09:04:07 -0400

    RAN 46B

--- Problem46B.cpp
+++ Problem46B.cpp
@@ -52,8 +52,8 @@ int main() {
         for(int b = 0; b < 5; b++) {
             if(si[b] != 0) {
                 int d = b - k;
+                cout<<d<<"\n";
                 if(abs(d) < mini.f && b > mini.s) {
-                    cout<<d<<"\n";
                     mini.f = abs(d);
                     mini.s = b;
                 }
