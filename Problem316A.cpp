a107976887db9c1c96c43020dfc96fa7861e2eb1
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-30 10:17:19 -0400

    RAN 316A

--- Problem316A.cpp
+++ Problem316A.cpp
@@ -34,14 +34,27 @@ int main() {
     string a;
     cin >> a;
     ll perm = 1;
+    int n = 0;
     mc letters;
-    if((a[0] >= 'a'&& a[0] <= 'z')||(a[0] >= 'A'&& a[0] <= 'Z')) {
+    if((a[0] >= 'a'&& a[0] <= 'z')||(a[0] >= 'A'&& a[0] <= 'Z') || a[0] == '?') {
         perm = 9;
-
+        letters[a[0]] = 9;
+        n++;
     }
-    for(int i = 1; i <a.length(); i++) {
-        
+    for(int i = 1; i < a.length(); i++) {
+        if((a[i] >= 'a'&& a[i] <= 'z')||(a[i] >= 'A'&& a[i] <= 'Z')) {
+            if(letters[a[i]] != 0) {
+                perm *= letters[a[i]];
+            } else {
+                perm *= n - 10;
+                letters[a[i]] = n - 10;
+                n++;
+            }
+        } else if (a[i] == '?') {
+            perm *= 10;
+        }
     }
+    cout<<perm;
     return 0;
 }
 
