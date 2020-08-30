33171231320c9de6be6fbc1e5188343b62a15607
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-30 15:04:06 -0400

    CLEANED 1231C

--- Problem1231C.cpp
+++ Problem1231C.cpp
@@ -45,14 +45,13 @@ int main() {
         for(int k = 0; k < m; k++) {
             if(mat[i][k] == 0) {
                 mat[i][k] = min(mat[i+1][k]-1, mat[i][k+1]-1);
-            }
-
-            int diffv = mat[i+1][k] - mat[i-1][k];
-            int diffh = mat[i][k+1] - mat[i][k-1];
-            cout<<diffh<<"\t"<<diffv<<"\n";
-            if(diffv <= 1 || diffh <= 1) {
-                cout<<-1;
-                return 0;
+                int diffv = mat[i+1][k] - mat[i-1][k];
+                int diffh = mat[i][k+1] - mat[i][k-1];
+                cout<<diffh<<"\t"<<diffv<<"\n";
+                if(diffv <= 1 || diffh <= 1) {
+                    cout<<-1;
+                    return 0;
+                }
             }
             sum += mat[i][k];
         }
