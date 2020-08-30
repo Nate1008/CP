38358e8994e6992f8b2c2c3de53cf028db25e11e
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-29 20:19:03 -0400

    RAN 46B

--- Problem46B.cpp
+++ Problem46B.cpp
@@ -30,7 +30,7 @@ using namespace std;
 
 int main() {
     vt<string> sizes;
-    sizes = {"S", "M", "L", "XL", "XXL"} ;
+    sizes = { "S", "M", "L", "XL", "XXL" };
     vt<string>::iterator it;
     map<int, int> si;
     for(int i = 0; i < 5; i++) {
