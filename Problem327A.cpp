2147157062e01bbc9b5ea7d2a9462a734f69c5cd
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-31 19:30:11 -0400

    RAN 327A

--- Problem327A.cpp
+++ Problem327A.cpp
@@ -44,11 +44,12 @@ int main() {
     }
     int mo = 0;
     for(int i = 0; i < n; i++) {
-        for(int k = i+1; k < n; k++) {
+        for(int k = i+1; k < n+1; k++) {
             int ones = 0;
-            for(int m = i; m < k; m++) {
+            for(int m = 0; m < k; m++) {
                 cout<<b[m]<<"\t";
-                if(b[m] == 0) {
+                b[m]
+                if() {
                     ones++;
                 }
             }
