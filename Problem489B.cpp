654107fd938165e1945e8a0b675cc5f6e2dfd8c6
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-31 15:54:16 -0400

    RAN 489B

--- Problem489B.cpp
+++ Problem489B.cpp
@@ -36,6 +36,34 @@
 using namespace std;
 
 int main() {
+    int b;
+    cin >> b;
+    vi boys(b);
+    for(int i = 0; i < b; i++) {
+        cin >> boys[i];
+    }
+    int g;
+    cin >> g;
+    vi girls(g);
+    for(int i = 0; i < g; i++) {
+        cin >> girls[i];
+    }
+    sort(boys.begin(), boys.end());
+    int k = 0;
+    int n = 0;
+    int match = 0;
+    while(k > 0 && n < boys.size()) {
+        for(int i = n; i < boys.size(); i++) {
+            int dif = boys[i] - girls[k];
+            if(abs(dif) <= 1) {
+                match++;
+                n++;
+                break;
+            }
+        }
+        k++;
+    }
+    cout<<match;
     return 0;
 }
 
