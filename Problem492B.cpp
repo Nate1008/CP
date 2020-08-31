5c9ac4591e9d87fbf50754a0cb070ec0bf5fe4a2
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-31 14:14:12 -0400

    CREATED 492B

---
+++ Problem492B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <vector>
+#include <set>
+#include <bitset>
+#include <map>
+#include <deque>
+#include <string>
+
+#include <algorithm>
+#include <numeric>
+
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cmath>
+
+#define ll long long
+#define mi map<int, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define pi pair<int, int>
+#define f first
+#define s second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
