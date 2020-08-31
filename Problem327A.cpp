b91e3c3eb431bb8e87266597a6073d0a1d9dafc0
Nate1008 <pidgeypiggy1000@gmail.com>
2020-08-31 18:33:44 -0400

    RAN 327A

--- Problem327A.cpp
+++ Problem327A.cpp
@@ -36,6 +36,24 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    int o = 0;
+    int z = 0;
+    int c = 0;
+    for(int i = 0; i < n; i++) {
+        bool t;
+        cin >> t;
+        if(t) {
+            o++;
+            z = max(z, c);
+            c = 0;
+        } else {
+            c++;
+            z = max(z, c);
+        }
+    }
+    cout<<o+z;
     return 0;
 }
 
