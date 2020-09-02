501858f429588951ff04eedd17a7a57074b695a5
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-02 17:52:21 -0400

    CLEANED 1363A

--- Problem1363A.cpp
+++ Problem1363A.cpp
@@ -39,6 +39,40 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+    for(int q = 0; q < t; q++) {
+        int n, k;
+        cin >> n >> k;
+        int odd = 0;
+        int even = 0;
+        for(int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            if(x % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        if(odd == 0) {
+            cout<<"NO"<<"\n";
+            continue;
+        }
+
+        k--;
+        odd--;
+
+        if((k % 2 == 0) && (even >= k || odd >= k)) {
+            cout<<"YES"<<"\n";
+            continue;
+        } else if (even >= k) {
+            cout<<"YES"<<"\n";
+            continue;
+        }
+        cout<<"NO"<<"\n";
+
+    }
     return 0;
 }
 
