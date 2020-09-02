1e0275bbd1c8323b168d6762f26345a2292b38cc
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-02 18:31:46 -0400

    CLEANED 1363A

--- Problem1363A.cpp
+++ Problem1363A.cpp
@@ -62,7 +62,7 @@ int main() {
 
         k -= even;
         
-        if(k < 0 || odd >= k){// && odd % 2 == 0)) {
+        if(k < 0 || odd > k){// && odd % 2 == 0)) {
             cout<<"YES"<<"\n";
             continue;
         }
