a3d370e86df3f1bef251f15a69ae34a5a09800c4
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-04 11:25:21 -0400

    RAN 1352B

--- Problem1352B.cpp
+++ Problem1352B.cpp
@@ -39,6 +39,48 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+    for(int q = 0; q < t; q++) {
+        double n, k;
+        cin >> n >> k;
+        int a = ceil(n/k);
+        int b = floor(n/k);
+        int ansa = 0;
+        int ansb = 0;
+        int half = ceil(k/2);
+        ansb = b-1*half + b+1*half-1;
+        ansa = a+1*half + a-1*half-1;
+        if(a == 1){
+            ansa = -1;
+        }
+        if(b == 1) {
+            ansb = -1;
+        }
+
+        if(ansa == n) {
+            cout<<"YES"<<"\n";       
+            for(int i = 0; i < half; i++) {
+                cout<<a+1<<" ";
+            }
+            for(int i = 0; i < half-1; i++) {
+                cout<<a-1<<" ";
+            }
+            cout<<"\n";
+            continue;
+        } else if (ansb == n) {
+            cout<<"YES"<<"\n";       
+            for(int i = 0; i < half; i++) {
+                cout<<b-1<<" ";
+            }
+            for(int i = 0; i < half-1; i++) {
+                cout<<b+1<<" ";
+            }
+            cout<<"\n";
+            continue;
+        }
+        cout<<"NO"<<"\n";
+    }
     return 0;
 }
 
