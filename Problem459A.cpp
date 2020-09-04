ced8bb94cab5233d14ecd63e27d62e207db409ec
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-04 15:38:14 -0400

    RAN 459A

--- Problem459A.cpp
+++ Problem459A.cpp
@@ -43,10 +43,10 @@ int main() {
     pi a, b;
     cin >> a.f >> a.s >> b.f >> b.s;
     
-    int diffv = b.f - a.f;
-    int diffh = b.s - a.s;
+    int diffv = abs(b.f - a.f);
+    int diffh = abs(b.s - a.s);
 
-    if((diffv != 0 && diffh != 0) && abs(diffv) != abs(diffh)){
+    if((diffv != 0 && diffh != 0) && diffv != diffh){
         cout<<-1;
     }
     int side = 0;
