09f40b5d1372bb236c2363c751ad7f3b03b0093d
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-05 09:47:44 -0400

    RAN 1385C

--- Problem1385C.cpp
+++ Problem1385C.cpp
@@ -57,11 +57,11 @@ int main() {
         for(int i = n-1; i > 0; i--) {
             int a = n;
             for(int c = i-1; c >= 0; c--) {
-                a = c;
                 // cout<<nums[c]<<"\t";
                 if(!(nums[c] <= nums[c+1])) {
                     break;
                 }
+                a = c;
             }
             cout<<"\n"<<"M: "<<m<<"\n";
             m = min(m, a+1);
