4db95c1a6332f915386bfd411eaf2b768f62379f
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-06 19:14:14 -0400

    RAN 639A

--- Problem639A.cpp
+++ Problem639A.cpp
@@ -74,10 +74,13 @@ int main() {
                     min = v;
                 }
             }
+            cout<<"MIN: "<<online[min]<<"\n";      
             if(t[a] > online[min]) {
                 online[min] = t[a];
+                cout<<t[a]<<"\n";
             }
         } else if (online.size() < k){
+            cout<<t[a]<<"\n";      
             online.push_back(t[a]);
         }
     }
