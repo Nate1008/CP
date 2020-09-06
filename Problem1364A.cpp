1889508cf6b05e6dfd3d939447eb5a99a47c1ad8
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-06 09:57:52 -0400

    RAN 1364A

--- Problem1364A.cpp
+++ Problem1364A.cpp
@@ -60,55 +60,42 @@ int main() {
             int v;
             cin >> v;
             nums[i] = v;
-        }
-        int m = -1;
-        FOR(n, i) {
-            for(int j = 0; j <= i; j++){
-                int sum = 0;
-                int c = (i - j)+1;
-                // cout<<"C: "<<c<<'\n';
-                for(int v = j; v <= i; v++) {
-                    sum += nums[v];
-                }
-                if(sum % k != 0){
-                    m = max(m, c);
-                }
+            if(v % k != 0) {
+                pass = true;
+            }
+            sum += v;
+            sums[i] = v;
+            if(sums[i] % k != 0) {
+                ind = i;
             }
         }
-        cout<<m<<'\n';
-        //     if(v % k != 0) {
-        //         pass = true;
-        //     }
-        //     sum += v;
-        //     sums[i] = v;
-        //     if(sums[i] % k != 0) {
-        //         ind = i;
-        //     }
-        // }
-        // if(!pass) {
-        //     cout<<-1<<"\n";
-        //     continue;    
-        // } else if (sum % k != 0) {
-        //     cout<<n<<"\n";
-        //     continue;
-        // }
-        // int c = 0; 
-        // int cur = 0;
-        // int l = 0;
-        // FOR(n, i) {
-        //     if((cur+nums[i]) % k == 0) {
-        //         // cout<<"DIV BY K: "<<nums[i]<<"\n";
-        //         c = 0;
-        //         cur = 0;
-        //     }
-        //     c++;
-        //     cur += nums[i];
-        //     l = max(l, c);
-        //     // cout<<"CURRENT MAX: "<<cur<<"\n";     
-        //     // cout<<"COUNT: "<<c<<"\n"; 
-        //     // cout<<"LEN: "<<l<<"\n";
-        // }
-        // cout<<max(l, ind)<<'\n';
+        if(!pass) {
+            cout<<-1<<"\n";
+            continue;    
+        } else if (sum % k != 0) {
+            cout<<n<<"\n";
+            continue;
+        }
+        int c = 0; 
+        int cur = 0;
+        int l = 0;
+        FOR(n, i) {
+            if((cur+nums[i]) % k == 0) {
+                // cout<<"DIV BY K: "<<nums[i]<<"\n";
+                cur += nums[i];
+                if(i < n-1 && cur+nums[i+1] % k == 0) {
+                    c = 0;
+                    cur = 0;
+                }
+            }
+            c++;
+            cur += nums[i];
+            l = max(l, c);
+            // cout<<"CURRENT MAX: "<<cur<<"\n";     
+            // cout<<"COUNT: "<<c<<"\n"; 
+            // cout<<"LEN: "<<l<<"\n";
+        }
+        cout<<max(l, ind)<<'\n';
     }
     return 0;
 }
