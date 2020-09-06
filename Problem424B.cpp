4dfd4b854d9c4371e579ba8067fedc1ec17b5130
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-06 13:23:43 -0400

    CREATED 424B

---
+++ Problem424B.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <vector>
+#include <set>
+#include <bitset>
+#include <map>
+#include <deque>
+#include <string>
+
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <functional>
+
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cmath>
+#include <climits>
+
+#define ll long long
+#define mi map<int, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define pi pair<int, int>
+#define f first
+#define s second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define RFOR(t, q) for(int q = t; q > 0; q--)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+
+#define TC int t; cin >> t; FOR(t, q)
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
