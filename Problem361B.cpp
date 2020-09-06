51ebe7c85b0cfdf1e3103f0f8aca4baf363fbc7d
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-06 16:55:58 -0400

    RAN 361B

--- Problem361B.cpp
+++ Problem361B.cpp
@@ -62,11 +62,15 @@ int main() {
     }
 
     for(int i = k+2; i <= n; i++){
-        if(perm[i%n] != 0) {
+        if(i % 2 == 1) {
+            if(perm[i%n] != 0) {
             perm[i-1] = perm[i%n];
             perm[i%n] = i;
+            } else {
+                perm[i%n] = i;            
+            }
         } else {
-            perm[i%n] = i;            
+            perm[i-2] = i;
         }
     }
     
