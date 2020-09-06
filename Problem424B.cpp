6904cac4ab83330896416dd899328d113373c540
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-06 14:31:11 -0400

    RAN 424B

--- Problem424B.cpp
+++ Problem424B.cpp
@@ -72,7 +72,7 @@ int main() {
     FOR(n, i) {
         int p = coor[i].s;
         float t = coor[i].f;
-        cout<<"DIS: "<<printf("%.6f", t)<<" POP: "<<p<<"\n";
+        cout<<"DIS: "<<printf("%.9f", t)<<" POP: "<<p<<"\n";
         k += p;
         if(k >= 1000000 && !pass) {
             pass = true;
