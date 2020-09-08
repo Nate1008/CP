e78e7bc4bdf73d0137b675de1d5ab45775cd2532
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-08 14:18:00 -0400

    RAN 725B

--- Problem725B.cpp
+++ Problem725B.cpp
@@ -60,8 +60,8 @@ using namespace std;
 int main() {
     string s;
     cin >> s;
-    int t = 0;
-    ull k = s[0] - '0';
+    ll t = 0;
+    ll k = s[0] - '0';
     if(s[1] == 'f') {
         t += 1;
     } else if(s[1] == 'e') {
