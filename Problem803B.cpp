63deafc314dd64d54f27762c2e12ed8809074ac5
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-08 18:38:01 -0400

    RAN 803B

--- Problem803B.cpp
+++ Problem803B.cpp
@@ -75,8 +75,8 @@ int main() {
         }
         int a, b;
         int md;
-        a = upper_bound(ALL(zeros), i)-zeros.begin();
-        b = upper_bound(RALL(zeros), i)-zeros.crbegin();
+        a = lower_bound(ALL(zeros), i)-zeros.begin();
+        b = lower_bound(RALL(zeros), i)-zeros.rbegin();
         cout<<"A: "<<a<<" "<<zeros[a]<<" "<<"B: "<<b<<" "<<zeros[b]<<'\n';
         md = min(abs(i - zeros[a]), abs(i - zeros[b]));
         cout<<md<<"\n";
