f96d201b6b7bdab875d5a5cc029b04c2fb258e67
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-08 19:02:54 -0400

    RAN 803B

--- Problem803B.cpp
+++ Problem803B.cpp
@@ -61,7 +61,7 @@ int main() {
     cin >> n;
     vi dist(n);
     vi zeros;
-    zeros.push_back(INT_MIN);
+    zeros.push_back(INT_MAX);
     FOR(n, i) {
         cin >> dist[i];
         if(dist[i] == 0) {
