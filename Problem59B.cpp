da7d9fdff4b1fd9a13c3e8c5c5e289368d0cdea0
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-09 09:07:43 -0400

    CREATED 59B

---
+++ Problem59B.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <vector>
+#include <set>
+#include <bitset>
+#include <map>
+#include <deque>
+#include <string>
+
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <functional>
+
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cmath>
+#include <climits>
+
+#define ll long long
+#define pi pair<int, int>
+#define pii pair<pi, int>
+#define mi map<int, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define mpi map<pi, int>
+#define F first
+#define S second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+#define vpii vector<pii>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define RFOR(t, q) for(int q = t; q > 0; q--)
+#define FORE(t, q) for(int q = 0; q <= t; q++)
+#define RFORE(t, q) for(int q = t; q >= 0; q--)
+
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define RFORN(t, q, v) for(int q = t; q > v; q--)
+#define FOREN(t, q, v) for(int q = v; q <= t; q++)
+#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
+
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+
+#define TC int t; cin >> t; FOR(t, q)
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
