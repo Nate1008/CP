2532dc791d18cc5a69913b6d1287717138848282
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-10 10:19:36 -0400

    Updating...

--- Problem234A.cpp
+++ Problem234A.cpp
@@ -62,9 +62,6 @@
 using namespace std;
 
 int main() {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    
     return 0;
 }
 
--- TemplateFile.txt
+++ TemplateFile.txt
@@ -62,6 +62,9 @@
 using namespace std;
 
 int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
     return 0;
 }
 
