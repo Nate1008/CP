e62806a3e20923f4e668d85b5b7c42a43138f7b0
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-11 15:41:27 -0400

    RAN 88A

--- Problem88A.cpp
+++ Problem88A.cpp
@@ -74,8 +74,8 @@ int main() {
     FOR(12, i) {
         notes[note[i]] = i;
     }
-    int d1 = ((notes[b] - notes[a]))%12;
-    int d2 = ((notes[c] - notes[b]))%12;
+    int d1 = min(notes[b] - notes[a], notes[b] - (notes[a]+12));
+    int d2 = min(notes[c] - notes[b], notes[c] - (notes[b]+12));
     cout<<"D1: "<<d1<<"\t"<<"D2: "<<d2<<"\n";
     if(d1 == 4 && d2 == 3) {
         cout<<"major";
