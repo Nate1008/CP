40a488277a5ab832d8b743aa3fb9b4437c8ec381
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-14 17:57:28 -0400

    RAN 1409D

--- Problem1409D.cpp
+++ Problem1409D.cpp
@@ -74,7 +74,7 @@ int main() {
         cin >> n >> s;
         int l = n.length();
         ll over = pow(10, l+1);
-        ll extra = stoll(n) - over;
+        ll extra = over - stoll(n);
         int digits = 0;
         int dig = l+1;
         FOR(l, i) {
