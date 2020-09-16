a40ccf461f8a285267e6febcf82405c33619ab46
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-16 11:03:54 -0400

    RAN 1407B

--- Problem1407B.cpp
+++ Problem1407B.cpp
@@ -80,7 +80,7 @@ int main() {
         }
 
         FOR(n, i) {
-            int g = __gcd(arr[i], m);
+            int g = gcd(arr[i], m);
             ans[i] = pi(g, arr[i]);
         }
         sort(RALL(ans));
