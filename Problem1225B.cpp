7c22fddfcebf8bb3ee555b960677e5e9743cd3dd
Nate1008 <pidgeypiggy1000@gmail.com>
2020-09-17 07:44:32 -0400

    CREATED 1225B

--- Problem1225B.cpp
+++ Problem1225B.cpp
@@ -67,47 +67,8 @@
 
 using namespace std;
 
-int const MAXN = 1e5 + 5;
-int cnt[MAXN];
-
-main()
-{
-    int n, q, x, cnt2 = 0, cnt4 = 0;
-    char type;
-    cin >> n;
-
-    for (int i = 1; i <= n; ++i)
-    {
-        cin >> x;
-
-        cnt2 -= cnt[x] / 2;
-        cnt4 -= cnt[x] / 4;
-        cnt[x]++;
-        cnt2 += cnt[x] / 2;
-        cnt4 += cnt[x] / 4;
-    }
-
-    cin >> q;
-
-    for (int i = 1; i <= q; ++i)
-    {
-        cin >> type >> x;
-        cnt2 -= cnt[x] / 2;
-        cnt4 -= cnt[x] / 4;
-
-        if (type == '+')
-            cnt[x]++;
-        else
-            cnt[x]--;
-
-        cnt2 += cnt[x] / 2;
-        cnt4 += cnt[x] / 4;
-
-        if (cnt4 >= 1 && cnt2 >= 4)
-            cout << "YES" << '\n';
-        else
-            cout << "NO" << '\n';
-    }
-
+int main() {
     return 0;
 }
+
+
