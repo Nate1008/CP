033985f095e6e86b3d59182e070093978f449c96
Nate1008 <pidgeypiggy1000@gmail.com>
2020-12-08 17:01:24 -0500

    Updating...

---
+++ CodeForces/Problem1369B.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define pf pair<float, int>
+#define ps pair<string, int>
+#define pii pair<pi, int>
+#define mi map<int, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define mpi map<pi, int>
+#define F first
+#define S second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+#define vpf vector<pf>
+#define vpii vector<pii>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define RFOR(t, q) for(int q = t; q > 0; q--)
+#define FORE(t, q) for(int q = 0; q <= t; q++)
+#define RFORE(t, q) for(int q = t; q >= 0; q--)
+
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define RFORN(t, q, v) for(int q = t; q > v; q--)
+#define FOREN(t, q, v) for(int q = v; q <= t; q++)
+#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
+
+#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
+
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+
+#define B(v) v.begin()
+#define RB(v) v.rbegin()
+#define E(v) v.end()
+#define RE(v) v.rend()
+
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+
+#define TC int t; cin >> t; FOR(t, q)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    TC {
+    	int n; cin >> n;
+    	string s; cin >> s;
+    	
+    	int ones = count(ALL(s), '1');
+
+    	int a = 0; // ZERO
+    	int b = 0; // ONE
+		FOR(n, i) {
+			if(s[i] == '0') a++; 
+			else break;
+		}
+
+		RFOR(n-1, i) {
+			if(s[i] == '1') b++;
+			else break;
+		}
+
+		// cout << "1: " << b << endl;
+    	// cout << "0: " << a << endl;
+
+		FOR(a, i) {
+			cout << '0';
+		}
+
+		if(b != ones) {
+			cout << '0';
+		}
+
+		FOR(b, i) {
+			cout << '1';
+		}
+		cout << endl;
+    }
+
+
+    return 0;
+}
