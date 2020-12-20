1b100f222172ea5910484a67ffe8f07af0cd00b0
Nate1008 <pidgeypiggy1000@gmail.com>
2020-12-20 13:59:50 -0500

    Updating...

---
+++ CodeForces/B/Problem1463B.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define pf pair<float, int>
+#define ps pair<string, int>
+#define pii pair<pi, int>
+#define mi map<int, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define mpi map<pi, int>
+#define F first
+#define S second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+#define vpf vector<pf>
+#define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define RFOR(t, q) for(int q = t; q > 0; q--)
+#define FORE(t, q) for(int q = 0; q <= t; q++)
+#define RFORE(t, q) for(int q = t; q >= 0; q--)
+
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define RFORN(t, q, v) for(int q = t; q > v; q--)
+#define FOREN(t, q, v) for(int q = v; q <= t; q++)
+#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
+
+#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
+#define sz(v) (int)(v.size())
+
+
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+
+#define B(v) v.begin()
+#define RB(v) v.rbegin()
+#define E(v) v.end()
+#define RE(v) v.rend()
+
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+
+#define TC int t; cin >> t; FOR(t, q)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    TC {
+        int n; cin >> n;
+        vl arr(n);
+        ll o = 0, e = 0;
+        FOR(n, i) {
+            cin >> arr[i];
+            if (i % 2 == 0) e+=arr[i];
+            else o+=arr[i];
+        }
+        if (e <= o) {
+            FOR(n, i) {
+                if (i % 2 == 0) cout << 1 << " ";
+                else cout << arr[i] << " ";
+            }
+        } else {
+            FOR(n, i) {
+                if (i % 2 == 1) cout << 1 << " ";
+                else cout << arr[i] << " ";
+            }
+        }
+        cout <<  endl;
+
+
+    };
+
+    return 0;
+}
