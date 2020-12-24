35357ccd5066128bb9318d08a18671b1fd0b9cdc
Nate1008 <pidgeypiggy1000@gmail.com>
2020-12-24 09:45:54 -0500

    Updating...

---
+++ AtCoder/D/ProblemB185D.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define pf pair<float, int>
+#define ps pair<string, int>
+#define pii pair<pi, int>
+#define mi map<int, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define mpi map<pi, int>
+#define F first
+#define S second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+#define vpf vector<pf>
+#define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define RFOR(t, q) for(int q = t; q > 0; q--)
+#define FORE(t, q) for(int q = 0; q <= t; q++)
+#define RFORE(t, q) for(int q = t; q >= 0; q--)
+
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define RFORN(t, q, v) for(int q = t; q > v; q--)
+#define FOREN(t, q, v) for(int q = v; q <= t; q++)
+#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
+
+#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
+#define sz(v) (int)(v.size())
+
+
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+
+#define B(v) v.begin()
+#define RB(v) v.rbegin()
+#define E(v) v.end()
+#define RE(v) v.rend()
+
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+
+#define TC int t; cin >> t; FOR(t, q)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m, x = 1e9; cin >> n >> m;
+    vi b, diff;
+    FOR(m, i) {
+        int x;
+        cin >> x;
+        b.push_back(x);
+    }
+    if (m == 0) {
+        cout << 1 << endl;
+        return 0;
+    }
+    sort(ALL(b));
+    if (b.front() != 1) { b.insert(B(b), 0); }
+    if (b.back() != n) { b.push_back(n+1);}
+
+    FORN(b.size(), i, 1) {
+        int d = b[i]-b[i-1]-1;
+        if (d > 0) x = min(x, d);
+        diff.push_back(d);
+    }
+    int ans = 0;
+    FOR(diff.size(), i) {
+        ans += diff[i] / x + (diff[i] % x ? 1 : 0);
+    }
+    cout << ans << endl;
+
+
+    return 0;
+}
