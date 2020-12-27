4a864cb2d750fb72301d03de6cfc0b75a531f4f6
Nate1008 <pidgeypiggy1000@gmail.com>
2020-12-27 10:32:05 -0500

    Updating...

---
+++ CSES/DP/Question1/Problem1.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define pf pair<float, int>
+#define ps pair<string, int>
+#define pii pair<pi, int>
+#define mi map<int, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define mpi map<pi, int>
+#define F first
+#define S second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+#define vpf vector<pf>
+#define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORV(a, x) for (auto& a : x)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define B(v) v.begin()
+#define RB(v) v.rbegin()
+#define E(v) v.end()
+#define RE(v) v.rend()
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+
+#define TC int t; cin >> t; FOR(t, q)
+
+using namespace std;
+
+const int MOD = 1e9+7;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n; cin >> n;
+    int dp[n+1];
+    dp[0] = 1;
+    for(int i = 1; i <= n; i++) {
+        dp[i] = 0;
+        for(auto x : {1, 2, 3, 4, 5, 6}) {
+            if (x > i) continue;
+            dp[i] += dp[i-x];
+            dp[i] %= MOD;
+        }
+    }
+    cout << dp[n];
+    return 0;
+}
