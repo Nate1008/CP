bb05a2d399e4a028505e2eaa0d8e1f933708ddb8
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-07 18:41:07 -0500

    Updating...

--- CodeForces/E/Problem1472E.cpp
+++ CodeForces/E/Problem1472E.cpp
@@ -36,20 +36,50 @@
 #define RE(v) v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
+
 
 #define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
 
 void solve() {
-
-}
+	int n; cin >> n;
+	vpii r(n);
+	vi w(n, 1e9), p(n, -1);
+	vi ans(n, -1);
+	FOR(n, i) {
+		cin >> r[i].F.F >> r[i].F.S;
+		if (r[i].F.F > r[i].F.S) swap(r[i].F.F, r[i].F.S);
+		r[i].S = i;
+	}
+	sort(all(r));
+	int pt = -1, mn = r[0].F.S, idx = r[0].S; w[0] = mn, p[0] = idx;
+	for(int i = 1; i < n; i++) {
+		if (r[i].F.F != r[i-1].F.F) {
+			pt = i - 1;
+		}
+		if (pt >= 0) {
+			if (w[pt] < r[i].F.S) {
+				ans[r[i].S] = p[pt]+1;
+			}
+		}
+		if (r[i].F.S < mn) {
+			mn = r[i].F.S;
+			idx = r[i].S;
+		}
+		w[i] = mn;
+		p[i] = idx;
+	}
+	FOR(n, i) cout << ans[i] << " ";
+	cout << endl;
+}	
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    solve();
+    TC solve();
 
     return 0;
 }
