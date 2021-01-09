991e1f8b430fa259081b5d84f2e70a41b79d3377
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-09 12:15:04 -0500

    Updating...

--- CodeForces/B/Problem1445B.cpp
+++ CodeForces/B/Problem1445B.cpp
@@ -44,14 +44,15 @@
 using namespace std;
 
 void solve() {
-
+	int a, b, c, d; cin >> a >> b >> c >> d;
+	cout << max(a + b, c + d) << endl;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    solve();
+    TC solve();
 
     return 0;
 }
