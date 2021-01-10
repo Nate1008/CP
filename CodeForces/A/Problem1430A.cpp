76e0231f281d84f923921dad0ff9de119ac1cb74
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-10 09:23:32 -0500

    Updating...

--- CodeForces/A/Problem1430A.cpp
+++ CodeForces/A/Problem1430A.cpp
@@ -19,55 +19,46 @@
 #define vpi vector<pi>
 #define vpf vector<pf>
 #define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define RFOR(t, q) for(int q = t; q > 0; q--)
-#define FORE(t, q) for(int q = 0; q <= t; q++)
-#define RFORE(t, q) for(int q = t; q >= 0; q--)
-
 #define FORN(t, q, v) for(int q = v; q < t; q++)
-#define RFORN(t, q, v) for(int q = t; q > v; q--)
-#define FOREN(t, q, v) for(int q = v; q <= t; q++)
-#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
-
-#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
-
-#define ALL(v) v.begin(), v.end()
-#define RALL(v) v.rbegin(), v.rend()
-
+#define FORV(a, x) for (auto& a : x)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 #define B(v) v.begin()
 #define RB(v) v.rbegin()
 #define E(v) v.end()
 #define RE(v) v.rend()
-
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
 
-#define TC int t; cin >> t; FOR(t, q)
+
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
+
+void solve() {
+	int n, a = 0, b = 0, c = 0; cin >> n;
+	int k = n % 3;
+	if (k > 0 && n < 5) {cout << -1 << endl; return;}
+	if (k == 1) c++, n-=7;
+	if (k == 2) b++, n-=5;
+	a = n / 3;
+	cout << a << " " << b << " " << c << endl;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    TC {
-    	int n; cin >> n;
-    	ll ans = 0;
-
-    	ll mx = 0;
-    	FOR(n, i)  {
-    		ll x; cin >> x;
-    		x += ans;
-    		if (x >= mx) {
-    			mx = x;
-    		} else {
-    			ans += mx - x;
-    		}
-    	}
-
-    	cout << ans << endl;
-    }
+    TC solve();
 
     return 0;
 }
