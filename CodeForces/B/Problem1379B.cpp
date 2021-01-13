cf7472870a51c0964f46a4515656b68d6f26d617
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-13 18:37:05 -0500

    Updating...

--- CodeForces/B/Problem1379B.cpp
+++ CodeForces/B/Problem1379B.cpp
@@ -6,6 +6,7 @@
 #define ps pair<string, int>
 #define pii pair<pi, int>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
 #define mpi map<pi, int>
@@ -26,6 +27,7 @@
 #define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
 #define FORV(a, x) for (auto& a : x)
 #define sz(v) (int)(v.size())
 #define all(v) v.begin(), v.end()
@@ -36,20 +38,34 @@
 #define RE(v) v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
+
 
 #define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
 
 void solve() {
-	
-}	
+	int l, r, d; ll m; cin >> l >> r >> m;
+	int lim = r - l;
+	FORN(r+1, i, l) {
+		int mod = m % i;
+		if (m-mod <= 0 || mod > i-mod) d = i-mod;
+		else d = mod;
+		if (d > lim) continue;
+		int a = r-d, b = r;
+		if (d == mod && mod != i-mod) swap(a, b);
+		cout << i << " " << a << " " << b << endl;
+		return;
+	}
+	return;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    solve();
+    TC solve();
 
     return 0;
 }
