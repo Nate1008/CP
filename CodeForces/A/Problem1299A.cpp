c93472c0cc92204d30907c66d2bd9c709d86525c
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-17 14:17:20 -0500

    Updating...

---
+++ CodeForces/A/Problem1299A.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define pf pair<float, int>
+#define ps pair<string, int>
+#define pii pair<pi, int>
+#define mi map<int, int>
+#define ml map<long long, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define mpi map<pi, int>
+#define F first
+#define S second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+#define vpf vector<pf>
+#define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define FORV(a, x) for (auto& a : x)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define B(v) v.begin()
+#define RB(v) v.rbegin()
+#define E(v) v.end()
+#define RE(v) v.rend()
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
+
+
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+void solve() {
+	int n, m; cin >> n;
+	vi a(n);
+	FOR(n, i) cin >> a[i];
+	for(int k=31;k>=0;k--){
+		int x = 1<<k;
+		int cnt = 0;
+		int y = 0;
+		for(int i=0;i<N;i++) if(a[i]&x) cnt++, y = i;
+		if(cnt == 1){
+			swap(a[0], a[y]);
+			break;
+		}
+	}
+	for(int i=0;i<N;i++) printf("%d ", a[i]);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+
+    return 0;
+}
