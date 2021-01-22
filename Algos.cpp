54017ba7e26d75ab658c6c5d59cfeef8d523b1ff
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-22 13:10:11 -0500

    Updating...

---
+++ Algos.cpp
@@ -0,0 +1,62 @@
+// nCr - Start
+
+long long nCr(int n, int r) {
+    if(r > n - r) r = n - r;
+    long long ans = 1;
+    int i;
+
+    for(i = 1; i <= r; i++) {
+        ans *= n - r + i;
+        ans /= i;
+    }
+
+    return ans;
+}
+
+// nCr - End
+
+// sieve - Start
+
+vector<int> smallest_factor;
+vector<bool> prime;
+vector<int> primes;
+ 
+void sieve(int maximum) {
+    maximum = max(maximum, 1);
+    smallest_factor.assign(maximum + 1, 0);
+    prime.assign(maximum + 1, true);
+    prime[0] = prime[1] = false;
+    primes = {};
+ 
+    for (int p = 2; p <= maximum; p++)
+        if (prime[p]) {
+            smallest_factor[p] = p;
+            primes.push_back(p);
+ 
+            for (int64_t i = int64_t(p) * p; i <= maximum; i += p)
+                if (prime[i]) {
+                    prime[i] = false;
+                    smallest_factor[i] = p;
+                }
+        }
+}
+
+// sieve - End
+
+// fast_pow - Start
+
+ll fast_pow(ll a, ll p) {
+    ll res = 1;
+    while (p) {
+        if (p % 2 == 0) {
+            a = (a * a) % mod;
+            p /= 2;
+        } else {
+            res = (res * a) % mod;
+            p--;
+        }
+    }
+    return res;
+}
+
+// fast_pow - End
