f879c7c4363e72f8243e89edf46d29c8a9bc9bc4
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-22 13:10:00 -0500

    Updating...

--- CodeForces/E/Problem1462E.cpp
+++ CodeForces/E/Problem1462E.cpp
@@ -6,6 +6,7 @@
 #define ps pair<string, int>
 #define pii pair<pi, int>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
 #define mpi map<pi, int>
@@ -26,7 +27,7 @@
 #define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define FORV(a, x) for (auto& a : x)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
 #define sz(v) (int)(v.size())
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
@@ -36,63 +37,49 @@
 #define RE(v) v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
+
 
 #define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
-int const N = 300500;
-ll fact[N], invFact[N];
-int const MOD = 1e9 + 7;
-
-ll fast_pow(ll a, ll p) {
-    ll res = 1;
-    while (p) {
-        if (p % 2 == 0) {
-            a = (a * a) % MOD;
-            p /= 2;
-        } else {
-            res = (res * a) % MOD;
-            p--;
-        }
-    }
-    return res;
-}
-
-ll C(int n, int k) {
-    if (k > n) {
-        return 0;
-    }
-    return fact[n] * invFact[k] % MOD * invFact[n - k] % MOD;
-}
 
 void solve() {
-    int n, m, k; cin >> n >> m >> k;
-    ll ans = 0;
-    vi a(n);
-    FOR(n, i) cin >> a[i];
-    sort(all(a));
-    for(int i = 0; i < n; i++) {
-        int l = i + 1;
-        int r = upper_bound(all(a), a[i]+k) - B(a);
-        ans += C(r - l, m - 1);
-        ans %= MOD;
-    }
-    cout << ans << endl;
+  int n;
+  cin >> n;
+  vector<ll> cnt(n + 1);
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    cnt[x]++;
+  }
+  ll ans = 0;
+  for (int i = 2; i < n; i++) {
+    ans += cnt[i - 1] * cnt[i] * cnt[i + 1];
+  }
+  for (int i = 1; i < n; i++) {
+    ans += cnt[i] * (cnt[i] - 1) / 2 * cnt[i + 1];
+  }
+  for (int i = 2; i <= n; i++) {
+    ans += cnt[i - 1] * cnt[i] * (cnt[i] - 1) / 2;
+  }
+  for (int i = 2; i < n; i++) {
+    ans += cnt[i - 1] * cnt[i + 1] * (cnt[i + 1] - 1) / 2;
+  }
+  for (int i = 2; i < n; i++) {
+    ans += cnt[i + 1] * cnt[i - 1] * (cnt[i - 1] - 1) / 2;
+  }
+  for (int i = 1; i <= n; i++) {
+    ans += cnt[i] * (cnt[i] - 1) * (cnt[i] - 2) / 6;
+  }
+  cout << ans << "\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    fact[0] = invFact[0] = 1;
-    for (int i = 1; i < N; i++) {
-        fact[i] = (fact[i - 1] * i) % MOD;
-        invFact[i] = fast_pow(fact[i], MOD - 2);
-    }
-
-    TC solve();
+    TC
+    	solve();
 
     return 0;
 }
-
-
