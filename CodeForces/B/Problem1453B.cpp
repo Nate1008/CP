bb7dea3ab49d0af688f8315af90246446d1f2ae1
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-23 11:15:43 -0500

    Updating...

---
+++ CodeForces/B/Problem1453B.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define pf pair<float, int>
+#define ps pair<string, int>
+#define pii pair<pi, int>
+#define mi map<int, int>
+#define ml map<long long, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define mpi map<pi, int>
+#define F first
+#define S second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+#define vpf vector<pf>
+#define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define B(v) v.begin()
+#define RB(v) v.rbegin()
+#define E(v) v.end()
+#define RE(v) v.rend()
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
+
+
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+const int MAX_N = 200000;
+int a[MAX_N + 5];
+void solve() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+ 
+	ll ans = 0;
+	for (int i = 2; i <= n; i++)
+		ans += abs(a[i] - a[i - 1]);
+ 
+	int mx = max(abs(a[1] - a[2]), abs(a[n] - a[n - 1]));
+	for (int i = 2; i < n; i++)
+		mx = max(mx, abs(a[i] - a[i - 1]) + abs(a[i + 1] - a[i]) - abs(a[i + 1] - a[i - 1]));
+ 
+	cout << ans - mx << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
+
+    return 0;
+}
