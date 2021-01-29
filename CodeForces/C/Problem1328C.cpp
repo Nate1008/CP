d44e95de84dcbe5a15a2f6189b35db7aaae3e6ef
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-29 16:16:32 -0500

    Updating...

--- CodeForces/C/Problem1328C.cpp
+++ CodeForces/C/Problem1328C.cpp
@@ -1,70 +1,68 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-
-#include <vector>
-#include <set>
-#include <bitset>
-#include <map>
-#include <deque>
-#include <string>
-
-#include <algorithm>
-#include <numeric>
-#include <iterator>
-#include <functional>
-
-#include <cstdio>
-#include <cassert>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <cmath>
-#include <climits>
+#include <bits/stdc++.h>
 
 #define ll long long
+#define pi pair<int, int>
+#define pf pair<float, int>
+#define ps pair<string, int>
+#define pii pair<pi, int>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
-#define pi pair<int, int>
-#define f first
-#define s second
+#define mpi map<pi, int>
+#define F first
+#define S second
 #define vt vector
 #define vi vector<int>
 #define vd vector<double>
 #define vf vector<float>
 #define vl vector<long long>
+#define vpi vector<pi>
+#define vpf vector<pf>
+#define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
 
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
 
-int main() {
-    int t;
-    cin >> t;
-    for(int q = 0; q < t; q++) {
-        string a = "";
-        string b = "";
-        int l;
-        cin >> l;
-        string str;
-        cin >> str;
-        for(int i = 0; i < l; i++) {
-            int k = str[i] - 48;
-            if(a == b) {
-                int x = ceil(k / 2);
-                int y = k - x;
-                a += to_string(x);
-                b += to_string(y);
-            } else if(a > b) {
-                a += '0';
-                b += to_string(k);
-            } else {
-                a += to_string(k);
-                b += '0';
-            }
-        }
-        cout<<a<<"\n"<<b<<"\n"; 
-    }
-    return 0;
+bool bal = 0;
+void solve() {
+	bal = 0;
+	int n; cin >> n;
+	string s, a = "", b = ""; cin >> s;
+	FOR(n, i) {
+		if (bal) {
+			a += '0';
+			b += s[i];
+		} else {
+			if (s[i] == '2') a += '1', b += '1';
+			else if (s[i] == '0') a += '0', b += '0';
+			else a += '1', b += '0', bal = 1;
+		}
+	}
+	cout << a << nl << b << nl;
 }
 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
+
+    return 0;
+}
