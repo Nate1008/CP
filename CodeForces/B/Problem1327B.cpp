8511a46de04db9b3d50f54ea23d4838b29cb9c27
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-29 16:41:54 -0500

    Updating...

--- CodeForces/B/Problem1327B.cpp
+++ CodeForces/B/Problem1327B.cpp
@@ -6,6 +6,7 @@
 #define ps pair<string, int>
 #define pii pair<pi, int>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
 #define mpi map<pi, int>
@@ -19,70 +20,44 @@
 #define vpi vector<pi>
 #define vpf vector<pf>
 #define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define RFOR(t, q) for(int q = t; q > 0; q--)
-#define FORE(t, q) for(int q = 0; q <= t; q++)
-#define RFORE(t, q) for(int q = t; q >= 0; q--)
-
 #define FORN(t, q, v) for(int q = v; q < t; q++)
-#define RFORN(t, q, v) for(int q = t; q > v; q--)
-#define FOREN(t, q, v) for(int q = v; q <= t; q++)
-#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
-
-#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
-#define si(v) (int)(v.size())
-
-
-#define ALL(v) v.begin(), v.end()
-#define RALL(v) v.rbegin(), v.rend()
-
-#define B(v) v.begin()
-#define RB(v) v.rbegin()
-#define E(v) v.end()
-#define RE(v) v.rend()
-
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
 
-#define TC int t; cin >> t; FOR(t, q)
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
+const int N = 1e5+10;
+vl res(N);
+void solve() {
+	int n; ll k; cin >> n >> k;
+	string ans(n, 'a');
+	int idx = lower_bound(all(res), k) - res.begin();
+	int k1 = n - idx - 1;
+	int k2 = k1 + 1 + (res[idx] - k);
+	ans[k1] = 'b', ans[k2] = 'b';
+	cout << ans << nl;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    TC {
-    	int n; cin >> n;
-    	vi kin(n);
-    	vi dau(n);
-    	int cnt = 0;
-    	FOR(n, i) {
-    		int k; cin >> k;
-    		bool once = false;
-    		FOR(k, j) {
-    			int x; cin >> x;
-    			if(kin[x-1] != 1 && !once) {
-    				cnt++;
-    				dau[i] = 1;
-    				kin[x-1] = 1;
-    				once = true;
-    			}
-    		}
-    	}
-
-    	if (cnt < n) {
-    		cout << "IMPROVE" << endl;
-    		int d = find(ALL(dau), 0) - B(dau);
-    		int idx = find(ALL(kin), 0) - B(kin);
-    		cout << d+1 << " " << idx+1 << endl;
-    	} else {
-    		cout << "OPTIMAL" << endl;
-    	}
-
-
-    }
+    ll cnt = 0, k = 0;
+    while (cnt < 3e9) cnt += k, res[k] = cnt, k++;
+    TC
+    	solve();
 
     return 0;
 }
