2d935d70a8660bd48e1aa6c73645c65d4fe0e3a7
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-30 19:53:06 -0500

    Updating...

--- CodeForces/A/Problem1443A.cpp
+++ CodeForces/A/Problem1443A.cpp
@@ -6,6 +6,7 @@
 #define ps pair<string, int>
 #define pii pair<pi, int>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
 #define mpi map<pi, int>
@@ -26,32 +27,31 @@
 #define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define FORV(a, x) for (auto& a : x)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
 #define sz(v) (int)(v.size())
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
-#define B(v) v.begin()
-#define RB(v) v.rbegin()
-#define E(v) v.end()
-#define RE(v) v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
 #define pb(s) push_back(s);
 
-
+#define nl endl
 #define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
 
 void solve() {
-
+	int n; cin >> n;
+	int k = n*4;
+	FOR(n, i) cout << k - (2*i) << " ";
+	cout << nl;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    solve();
+    TC
+    	solve();
 
     return 0;
 }
