8a936a9cc4c5b217f39422e09b8a2513ee771077
Nate1008 <pidgeypiggy1000@gmail.com>
2021-01-31 09:09:04 -0500

    Updating...

---
+++ CodeForces/B/Problem716B.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define pf pair<float, int>
+#define ps pair<string, int>
+#define pii pair<pi, int>
+#define mi map<int, int>
+#define ml map<long long, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define mpi map<pi, int>
+#define F first
+#define S second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+#define vpf vector<pf>
+#define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+bool ok = false;
+
+void solve() {
+	string s; cin >> s;
+	int n = sz(s);
+	if (n < 26) {cout << -1 << nl; return;}
+	string ans = s;
+	mi cnt;
+	vi quest;
+	vt<char> rep;	 
+	FOR(n-25, i){
+		// cout << i << nl;
+		cnt.clear(), rep.clear(), quest.clear();
+		FORN(26+i, j, i) {
+			if (s[j] == '?') {quest.pb(j);}
+			else cnt[s[j]-'A']++;
+		}
+
+		FOR(26, j) {
+			char c = 'A'+j;
+			if (cnt[j] == 0) rep.pb(c);
+		}
+		ok = false;
+		// cout << sz(rep) << "::" << sz(quest) << nl;
+		if (sz(rep) == sz(quest)){ 
+			FOR(sz(rep), j) {
+				ans[quest[j]] = rep[j];
+			}
+			ok = true;
+			break;
+		}
+	}
+	FOR(n, i) {
+		if (ans[i] == '?') ans[i] = 'Z';
+	}
+	cout << (ok ? ans : "-1") << nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    // TC
+    	solve();
+
+    return 0;
+}
