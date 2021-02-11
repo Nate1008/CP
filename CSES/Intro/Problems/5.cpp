6cf2c0c0a0304de70d3ce322510bb7a98c69f8ce
Nate1008 <pidgeypiggy1000@gmail.com>
2021-02-11 16:41:26 -0500

    Updating...

--- CSES/Intro/Problems/5.cpp
+++ CSES/Intro/Problems/5.cpp
@@ -41,13 +41,16 @@
 using namespace std;
 
 void solve() {
-
+	int n; cin >> n;
+	if (n <= 3 && n != 1) {cout << "NO SOLUTION"; return;}
+	for(int i = 2; i <= n; i += 2) cout << i << " ";
+	for(int i = 1; i <= n; i += 2) cout << i << " ";
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    TC
+    // TC
     	solve();
 
     return 0;
