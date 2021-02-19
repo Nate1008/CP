7ddc90844bcfc075f69864fa9f763457f68dfad1
Nate1008 <pidgeypiggy1000@gmail.com>
2021-02-19 17:21:05 -0500

    Updating...

--- CodeForces/C/Problem1203C.cpp
+++ CodeForces/C/Problem1203C.cpp
@@ -4,8 +4,9 @@
 #define pi pair<int, int>
 #define pf pair<float, int>
 #define ps pair<string, int>
-#define pii pair<pi, int>
+#define pii pair<int, pi>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
 #define mpi map<pi, int>
@@ -25,61 +26,42 @@
 #define sf set<float>
 #define sl set<long long>
 
-
-
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define RFOR(t, q) for(int q = t; q > 0; q--)
-#define FORE(t, q) for(int q = 0; q <= t; q++)
-#define RFORE(t, q) for(int q = t; q >= 0; q--)
-
 #define FORN(t, q, v) for(int q = v; q < t; q++)
-#define RFORN(t, q, v) for(int q = t; q > v; q--)
-#define FOREN(t, q, v) for(int q = v; q <= t; q++)
-#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
-
-#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
 #define sz(v) (int)(v.size())
-
-
-#define ALL(v) v.begin(), v.end()
-#define RALL(v) v.rbegin(), v.rend()
-
-#define B(v) v.begin()
-#define RB(v) v.rbegin()
-#define E(v) v.end()
-#define RE(v) v.rend()
-
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
 
-#define TC int t; cin >> t; FOR(t, q)
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
+
+void solve() {
+	int n; cin >> n;
+	ll g = 0;
+	FOR(n, i) {
+		ll x; cin >> x;
+		g = gcd(g, x);
+	}
+	ll cnt = 0;
+	for(int i = 1; i*1LL*i <= g; i++) {
+		if (g % i == 0) {
+			cnt++;
+			if (i * 1LL * i != g) cnt++;
+		}
+	}
+	cout << cnt << nl;
+}	
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    int n;
-	cin >> n;
-	long long g = 0;
-	for (int i = 0; i < n; ++i) {
-		long long x;
-		cin >> x;
-		g = gcd(g, x);
-	}
-	
-	int ans = 0;
-	for (int i = 1; i * 1ll * i <= g; ++i) {
-		if (g % i == 0) {
-			++ans;
-			if (i != g / i) {
-				++ans;
-			}
-		}
-	}
-	cout << ans << endl;
-
+    // TC
+    	solve();
 
     return 0;
 }
