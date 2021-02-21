b3c070f9565e13bdd4d1b2e254184baa92015205
Nate1008 <pidgeypiggy1000@gmail.com>
2021-02-21 11:11:19 -0500

    Updating...

--- CodeForces/E/Problem1433E.cpp
+++ CodeForces/E/Problem1433E.cpp
@@ -4,8 +4,9 @@
 #define pi pair<int, int>
 #define pf pair<float, int>
 #define ps pair<string, int>
-#define pii pair<pi, int>
+#define pii pair<int, pi>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
 #define mpi map<pi, int>
@@ -26,52 +27,31 @@
 #define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define RFOR(t, q) for(int q = t; q > 0; q--)
-#define FORE(t, q) for(int q = 0; q <= t; q++)
-#define RFORE(t, q) for(int q = t; q >= 0; q--)
-
 #define FORN(t, q, v) for(int q = v; q < t; q++)
-#define RFORN(t, q, v) for(int q = t; q > v; q--)
-#define FOREN(t, q, v) for(int q = v; q <= t; q++)
-#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
-
-#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
 #define sz(v) (int)(v.size())
-
-
-#define ALL(v) v.begin(), v.end()
-#define RALL(v) v.rbegin(), v.rend()
-
-#define B(v) v.begin()
-#define RB(v) v.rbegin()
-#define E(v) v.end()
-#define RE(v) v.rend()
-
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
 
-#define TC int t; cin >> t; FOR(t, q)
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
 
-const int N = 21;
-ll f[N];
+const vl ans = {1, 3, 40, 1260, 72576, 6652800, 889574400, 163459296000, 39520825344000, 12164510040883200};
+
+void solve() {
+	int n; cin >> n;
+	cout << ans[(n/2)-1];
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    // TC
+    	solve();	
 
-    int n;
-    cin >> n;
-    f[0] = 1;
-    for (int i = 1; i < N; ++i) {
-        f[i] = f[i - 1] * i;
-    }
-
-    long long ans = f[n] / f[n / 2] / f[n / 2];
-    ans = ans * f[n / 2 - 1];
-    ans = ans * f[n / 2 - 1];
-    ans /= 2;
-
-    cout << ans << endl;
     return 0;
 }
