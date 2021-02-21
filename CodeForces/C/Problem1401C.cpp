91852959434ba15a501520ac4a51fbd684d66d60
Nate1008 <pidgeypiggy1000@gmail.com>
2021-02-21 11:18:34 -0500

    Updating...

--- CodeForces/C/Problem1401C.cpp
+++ CodeForces/C/Problem1401C.cpp
@@ -4,8 +4,9 @@
 #define pi pair<int, int>
 #define pf pair<float, int>
 #define ps pair<string, int>
-#define pii pair<pi, int>
+#define pii pair<int, pi>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
 #define mpi map<pi, int>
@@ -26,53 +27,41 @@
 #define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define RFOR(t, q) for(int q = t; q > 0; q--)
-#define FORE(t, q) for(int q = 0; q <= t; q++)
-#define RFORE(t, q) for(int q = t; q >= 0; q--)
-
 #define FORN(t, q, v) for(int q = v; q < t; q++)
-#define RFORN(t, q, v) for(int q = t; q > v; q--)
-#define FOREN(t, q, v) for(int q = v; q <= t; q++)
-#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
-
-#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
 #define sz(v) (int)(v.size())
-
-
-#define ALL(v) v.begin(), v.end()
-#define RALL(v) v.rbegin(), v.rend()
-
-#define B(v) v.begin()
-#define RB(v) v.rbegin()
-#define E(v) v.end()
-#define RE(v) v.rend()
-
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
 
-#define TC int t; cin >> t; FOR(t, q)
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
+	
+const int INF = 1e9+10;
+bool ok = false;
+void solve() {
+	int n; cin >> n;
+	int mn = INF;
+	vi a(n), c;
+	FOR(n, i) {cin >> a[i]; mn = min(mn, a[i]);}
+	vi sorted = a;
+	sort(all(sorted));
+	FOR(n, i) if (a[i] % mn) c.pb(i);
+	ok = true;
+	for(auto i : c) {
+		ok &= sorted[i] == a[i];
+	}
+	cout << (ok ? "YES" : "NO") << nl;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    TC {
-        int n; cin >> n;
-        vi a(n);
-        FOR(n, i) cin >> a[i];
-        vi b = a;
-        sort(ALL(b));
-        vi comp(n, 0);
-        FOR(n, i) if (a[i] != b[i]) {
-                if (b[i] % b[0]) goto L;
-        }
-
-        cout << "YES" << endl;
-        continue;
-        L: cout << "NO" << endl;
-    };
+    TC
+    	solve();
 
     return 0;
 }
