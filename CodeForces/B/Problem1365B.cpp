98ba21b618f39ff2450d0b148d1506bce86759c1
Nate1008 <pidgeypiggy1000@gmail.com>
2021-02-22 18:08:43 -0500

    Updating...

--- CodeForces/B/Problem1365B.cpp
+++ CodeForces/B/Problem1365B.cpp
@@ -4,8 +4,9 @@
 #define pi pair<int, int>
 #define pf pair<float, int>
 #define ps pair<string, int>
-#define pii pair<pi, int>
+#define pii pair<int, pi>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
 #define mpi map<pi, int>
@@ -19,74 +20,47 @@
 #define vpi vector<pi>
 #define vpf vector<pf>
 #define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define RFOR(t, q) for(int q = t; q > 0; q--)
-#define FORE(t, q) for(int q = 0; q <= t; q++)
-#define RFORE(t, q) for(int q = t; q >= 0; q--)
-
 #define FORN(t, q, v) for(int q = v; q < t; q++)
-#define RFORN(t, q, v) for(int q = t; q > v; q--)
-#define FOREN(t, q, v) for(int q = v; q <= t; q++)
-#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
-
-#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
-
-#define ALL(v) v.begin(), v.end()
-#define RALL(v) v.rbegin(), v.rend()
-
-#define B(v) v.begin()
-#define RB(v) v.rbegin()
-#define E(v) v.end()
-#define RE(v) v.rend()
-
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
 
-#define TC int t; cin >> t; FOR(t, q)
-
-#define endl '\n'
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
+
+void solve() {
+	int n; cin >> n;
+	vi a(n);
+	si ok;
+	FOR(n, i) cin >> a[i];
+	FOR(n, i) {
+		int x; cin >> x;
+		ok.insert(x);
+	}
+
+	vi c = a;
+	sort(all(c));
+	if (c == a || sz(ok) == 2) cout << "Yes" << nl; 
+	else cout << "No" << nl;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    TC {
-    	int n; cin >> n;
-    	vi a(n);
-    	FOR(n, i) {
-    		cin >> a[i];
-    	}
-
-    	vi b(n);
-    	bool z = false;
-    	bool o = false;
-    	FOR(n, i) {
-    		cin >> b[i];
-    		if (b[i] == 0) {
-    			z = true;
-    		} else if (b[i] == 1) {
-    			o = true;
-    		}
-    	}
-
-    	vi p = a;
-    	sort(ALL(p));
-
-    	if (p == a) {
-    		cout << "Yes" << endl;
-    		continue;
-    	}
-
-    	if (z && o) {
-    		cout << "Yes" << endl;
-    	} else {
-    		cout << "No" << endl;
-    	}
-    	
-    }
+    TC
+    	solve();
 
     return 0;
 }
