0b095398a5d514186755db5762df19d2b59ccdba
Nate1008 <pidgeypiggy1000@gmail.com>
2021-02-23 11:26:55 -0500

    Updating...

--- CodeForces/D/Problem1360D.cpp
+++ CodeForces/D/Problem1360D.cpp
@@ -4,8 +4,9 @@
 #define pi pair<int, int>
 #define pf pair<float, int>
 #define ps pair<string, int>
-#define pii pair<pi, int>
+#define pii pair<int, pi>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
 #define mpi map<pi, int>
@@ -26,48 +27,36 @@
 #define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define RFOR(t, q) for(int q = t; q > 0; q--)
-#define FORE(t, q) for(int q = 0; q <= t; q++)
-#define RFORE(t, q) for(int q = t; q >= 0; q--)
-
 #define FORN(t, q, v) for(int q = v; q < t; q++)
-#define RFORN(t, q, v) for(int q = t; q > v; q--)
-#define FOREN(t, q, v) for(int q = v; q <= t; q++)
-#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
-
-#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
 #define sz(v) (int)(v.size())
-
-
-#define ALL(v) v.begin(), v.end()
-#define RALL(v) v.rbegin(), v.rend()
-
-#define B(v) v.begin()
-#define RB(v) v.rbegin()
-#define E(v) v.end()
-#define RE(v) v.rend()
-
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
 
-#define TC int t; cin >> t; FOR(t, q)
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
+
+const int INF = 1e9+10;
+void solve() {
+	int n, k, ans = INF; cin >> n >> k;
+	for(int i = 1; i*i <= n; i++) {
+		if (n % i == 0) {
+			if (i <= k) ans = min(ans, (n/i));
+			if (n/i <= k) ans = min(ans, i);
+		}
+	}
+	cout << ans << nl;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    TC
+    	solve();
 
-    TC {
-        int n, k; cin >> n >> k;
-        int ans = n;
-        for (int i = 1; i*i <= n; i++) {
-            if (n % i == 0) {
-                if (i <= k) ans = min(ans, n / i);
-                if (n/i <= k) ans = min(ans, i);
-            }
-        }
-        cout << ans << endl;
-    }
     return 0;
 }
