879c0fbf695c137cb035e37c1a2faf98fcc3c809
Nate1008 <pidgeypiggy1000@gmail.com>
2021-02-27 10:05:52 -0500

    Updating...

--- CodeForces/B/Problem1364B.cpp
+++ CodeForces/B/Problem1364B.cpp
@@ -4,8 +4,9 @@
 #define pi pair<int, int>
 #define pf pair<float, int>
 #define ps pair<string, int>
-#define pii pair<pi, int>
+#define pii pair<int, pi>
 #define mi map<int, int>
+#define ml map<long long, int>
 #define ms map<string, int>
 #define mc map<char, int>
 #define mpi map<pi, int>
@@ -26,54 +27,40 @@
 #define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define RFOR(t, q) for(int q = t; q > 0; q--)
-#define FORE(t, q) for(int q = 0; q <= t; q++)
-#define RFORE(t, q) for(int q = t; q >= 0; q--)
-
 #define FORN(t, q, v) for(int q = v; q < t; q++)
-#define RFORN(t, q, v) for(int q = t; q > v; q--)
-#define FOREN(t, q, v) for(int q = v; q <= t; q++)
-#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
-
-#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
 #define sz(v) (int)(v.size())
-
-
-#define ALL(v) v.begin(), v.end()
-#define RALL(v) v.rbegin(), v.rend()
-
-#define B(v) v.begin()
-#define RB(v) v.rbegin()
-#define E(v) v.end()
-#define RE(v) v.rend()
-
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
 
-#define TC int t; cin >> t; FOR(t, q)
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
 
-int p[100005];
-int main()
-{
-    int t;
-    scanf("%d",&t);
-    while (t--)
-    {
-        int n;
-        scanf("%d",&n);
-        for (int i=1;i<=n;i++)
-        scanf("%d",&p[i]);
-        vector<int> ans;
-        for (int i=1;i<=n;i++)
-        {
-            if (i==1 || i==n || (p[i-1]<p[i])!=(p[i]<p[i+1]))
-            ans.push_back(p[i]);
-        }
-        printf("%d\n",sz(ans));
-        FOR(sz(ans), i)
-        printf("%d ",ans[i]);
-        printf("\n");
-    }
+void solve() {
+	int n; cin >> n;
+	vi a(n), ans;
+	FOR(n, i) cin >> a[i];
+	ans.pb(a[0]);
+	int i = 1, j = i;
+	while(i < n) {
+		while(j < n-1 && (ans.back() < a[i]) == (a[j] < a[j+1])) j++;
+		ans.pb(a[j]);
+		i = j+1;
+	}
+	cout << sz(ans) << nl;
+	for(auto c : ans) cout << c << " ";
+	cout << nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
+
+    return 0;
 }
