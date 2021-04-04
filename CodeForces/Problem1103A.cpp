2d7898b20480b547945024a651a3fba473b4a6d9
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-04 16:26:15 -0400

    Updating...

---
+++ CodeForces/Problem1103A.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define pf pair<float, int>
+#define ps pair<string, int>
+#define pii pair<int, pi>
+#define mi map<int, int>
+#define ml map<long long, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define mpi map<pi, int>
+#define F first
+#define S second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+#define vpf vector<pf>
+#define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+vpi hor = {pi(1, 1), pi(2, 1), pi(3, 1), pi(4, 1)}, ver = {pi(1, 3), pi(3, 3), pi(1, 4), pi(3, 4)};
+
+void solve() {
+	string s; cin >> s;
+	int cnth = 0, cntv = 0;
+	for(auto c : s) {
+		if (c == '0') {
+			cout << ver[cntv].F << " " << ver[cntv].S << nl;
+			cntv++;
+			cntv %= 4;
+		} else {
+			cout << hor[cnth].F << " " << hor[cnth].S << nl;
+			cnth++;
+			cnth %= 4;
+		}
+	}
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    // TC
+    	solve();
+
+    return 0;
+}
