f775499f5fd3793de9740888ad80d866661786b4
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-05 12:19:25 -0400

    Updating...

---
+++ CodeForces/F/Problem1490F.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define pf pair<float, int>
+#define ps pair<string, int>
+#define pii pair<int, pi>
+#define mi map<int, int>
+#define ml map<long long, int>
+#define ms map<string, int>
+#define mc map<char, int>
+#define mpi map<pi, int>
+#define F first
+#define S second
+#define vt vector
+#define vi vector<int>
+#define vd vector<double>
+#define vf vector<float>
+#define vl vector<long long>
+#define vpi vector<pi>
+#define vpf vector<pf>
+#define vpii vector<pii>
+#define st set
+#define si set<int>
+#define sd set<double>
+#define sf set<float>
+#define sl set<long long>
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define pb(s) push_back(s);
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+const int INF = 1e9+10;
+void solve() {
+	int n; cin >> n;
+	map<int, int> m;
+	FOR(n, i) {
+		int x; cin >> x;
+		m[x]++;
+	}
+
+	set<int> C;
+	for(auto [k, v] : m) {
+		C.insert(v);
+	}
+
+	int ans = INF;
+	for(auto c : C) {
+		int cnt = 0;
+		for(auto [k, v] : m) {
+			if (v >= c) {
+				cnt += (v-c);
+			} else cnt += v;
+		}
+		ans = min(ans, cnt);
+	}
+	cout << ans << nl;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
+
+    return 0;
+}
