45b64a42642026d43ed3dacc19ea7367b668c466
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-09 13:27:48 -0400

    Updating...

--- CodeForces/D/Problem1409D.cpp
+++ CodeForces/D/Problem1409D.cpp
@@ -1,104 +1,69 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-
-#include <vector>
-#include <set>
-#include <bitset>
-#include <map>
-#include <deque>
-#include <string>
-
-#include <algorithm>
-#include <numeric>
-#include <iterator>
-#include <functional>
-
-#include <cstdio>
-#include <cassert>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <cmath>
-#include <climits>
+#include <bits/stdc++.h>
 
 #define ll long long
 #define pi pair<int, int>
-#define pf pair<float, int>
-#define ps pair<string, int>
-#define pii pair<pi, int>
 #define mi map<int, int>
-#define ms map<string, int>
-#define mc map<char, int>
-#define mpi map<pi, int>
-#define F first
-#define S second
+#define fi first
+#define se second
 #define vt vector
 #define vi vector<int>
-#define vd vector<double>
-#define vf vector<float>
 #define vl vector<long long>
-#define vpi vector<pi>
-#define vpf vector<pf>
-#define vpii vector<pii>
+#define st set
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define RFOR(t, q) for(int q = t; q > 0; q--)
-#define FORE(t, q) for(int q = 0; q <= t; q++)
-#define RFORE(t, q) for(int q = t; q >= 0; q--)
-
 #define FORN(t, q, v) for(int q = v; q < t; q++)
-#define RFORN(t, q, v) for(int q = t; q > v; q--)
-#define FOREN(t, q, v) for(int q = v; q <= t; q++)
-#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
 
-#define ALL(v) v.begin(), v.end()
-#define RALL(v) v.rbegin(), v.rend()
-
-#define B(v) v.begin()
-#define RB(v) v.rbegin()
-#define E(v) v.end()
-#define RE(v) v.rend()
-
-#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
-#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
-
-#define TC int t; cin >> t; FOR(t, q)
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
 
+int sum(ll x) {
+	int ret = 0;
+	while(x) {
+		ret += (x%10);
+		x /= 10;
+	}
+	return ret;
+}
+int d[19];
+void solve() {
+	ll n, s; cin >> n >> s;
+	ll x = n;
+	for(auto& c : d) c = 0;
+	int nxt = 0;
+	while(x) {
+		d[nxt] = (x%10);
+		x /= 10;
+		nxt++;
+	}
+
+	if (sum(n) <= s) {
+		cout << 0 << nl;
+		return;
+	}
+
+	ll cur = 0, mul = 1;
+	for(int i = 0; i < 19; i++, mul *= 10) {
+		cur += (d[i]*mul);
+		ll dif = (mul*10) - cur;
+		if (sum(n+dif) <= s) {
+			cout << dif << nl;
+			return;
+		}
+	}
+	cout << -1 << nl;
+}	
+
 int main() {
-    TC {
-        string n;
-        int s;
-        cin >> n >> s;
-        int l = n.length();
-        int digits = 0;
-        int dig = l+1;
-        bool pass = true;
-        FOR(l, i) {
-            digits += n[i] - '0';
-            if(digits >= s && pass) {
-                dig = i;
-                pass = false;
-            }
-        }
-        if(digits <= s) {
-            cout<<0<<"\n";
-            continue;
-        }
-        ll over = pow(10, l);
-        ll extra = over - stoll(n);
-        // dig -= 1;
-        ll in = pow(10,(l-dig));
-        extra %= in;
-        cout<<extra<<"\n";
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
 
-        
-
-
-    }
     return 0;
 }
-
-
