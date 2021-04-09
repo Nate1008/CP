5c31b2712d841567c42c1a1918cc35eaf566aef3
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-09 13:28:09 -0400

    Updating...

--- Template.cpp
+++ Template.cpp
@@ -2,37 +2,19 @@
 
 #define ll long long
 #define pi pair<int, int>
-#define pf pair<float, int>
-#define ps pair<string, int>
-#define pii pair<int, pi>
 #define mi map<int, int>
-#define ml map<long long, int>
-#define ms map<string, int>
-#define mc map<char, int>
-#define mpi map<pi, int>
-#define F first
-#define S second
+#define fi first
+#define se second
 #define vt vector
 #define vi vector<int>
-#define vd vector<double>
-#define vf vector<float>
 #define vl vector<long long>
-#define vpi vector<pi>
-#define vpf vector<pf>
-#define vpii vector<pii>
 #define st set
-#define si set<int>
-#define sd set<double>
-#define sf set<float>
-#define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
 #define FORN(t, q, v) for(int q = v; q < t; q++)
 #define sz(v) (int)(v.size())
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
-#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
-#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
 #define pb(s) push_back(s);
 
 #define nl endl
@@ -42,7 +24,7 @@ using namespace std;
 
 void solve() {
 
-}
+}	
 
 int main() {
     ios_base::sync_with_stdio(0);
