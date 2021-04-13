2863992f412eb6bbcc3e35454197396a766b2be3
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-13 09:30:38 -0400

    Updating...

---
+++ CodeForces/C/Problem1372C.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define fi first
+#define se second
+#define vt vector
+#define vi vector<int>
+#define vl vector<long long>
+#define st set
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+void solve() {
+	int n; cin >> n;
+	vi a(n);
+	for(auto& c : a) cin >> c;
+
+	vi b = a;
+	sort(all(b));
+	if (b == a) {
+		cout << 0 << nl;
+		return;
+	}
+
+	int cnt = 0;
+	for(int i = 0; i < n; i++) {
+		cnt += (a[i] == b[i]);
+	}
+
+	int j = n-1;
+	while(j >= 0 && a[j] == b[j]) j--, cnt--;
+	int i = 0;
+	while(i < j && a[i] == b[i]) i++, cnt--;
+
+	if (cnt == 0) {
+		cout << 1 << nl;
+	} else {
+		cout << 2 << nl;
+	}
+}		
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
+
+    return 0;
+}
