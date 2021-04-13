dfae9054aa0fcce4cb3a37da63d6a8d65ff2eeba
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-13 09:40:52 -0400

    Updating...

---
+++ CodeForces/B/Problem1368B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define fi first
+#define se second
+#define vt vector
+#define vi vector<int>
+#define vl vector<long long>
+#define st set
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+const string t = "codeforces";
+void solve() {
+	ll k; cin >> k;
+	ll ans = 1;
+	vi cnt(15, 1);
+	int i = 0;
+	while(ans < k) {
+		ans /= cnt[i]++;
+		ans *= cnt[i];
+		i++;
+		i %= sz(t);
+	}
+	string s = "";
+	for(int i = 0; i < sz(t); i++) {
+		for(int c = 0; c < cnt[i]; c++) {
+			s += t[i];
+		}
+	}
+	cout << s << nl;
+}		
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    // TC
+    	solve();
+
+    return 0;
+}
