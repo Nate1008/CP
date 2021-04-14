e1220bbfd2deda529dcd82fc7febeeba37d35bed
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-14 16:05:31 -0400

    Updating...

--- CodeForces/H/Problem1250H.cpp
+++ CodeForces/H/Problem1250H.cpp
@@ -22,8 +22,38 @@
 
 using namespace std;
 
-void solve() {
+const int MAX = 1e5+100;
+string ret(int v, int c) {
+	char ch = v+'0';
+	string s(c, ch); 
 
+	if (v == 0) {
+		s = '1'+s;
+	}
+
+	return s;
+}
+
+
+
+void solve() {
+	vi a(10);
+	for(auto& c : a) cin >> c;
+
+	a.pb(a.front());
+	a.erase(a.begin());
+
+	for(int x = 1; x <= MAX; x++) {
+		for(int i = 0; i < 10; i++) {
+			if (a[i] < x) {
+				int v = (i+1)%10;
+				cout << ret(v, x) << nl;
+				// cout << "DONE" << nl;
+				return;
+			}
+		}
+	}
+	cout << -1 << nl;
 }	
 
 int main() {
