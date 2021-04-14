52db980226618985e1bb1a58df7e19d59786fad2
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-14 11:22:21 -0400

    Updating...

---
+++ CodeForces/D/Problem1256D.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define fi first
+#define se second
+#define vt vector
+#define vi vector<int>
+#define vl vector<long long>
+#define st set
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+void solve() {
+	ll n, k; cin >> n >> k;
+	string s; cin >> s;
+	ll cnt = 0, nxt = 0;
+	for(int i = 0; i < n; i++) {
+		if (s[i] == '1') {
+			cnt++;
+			continue;
+		}
+		while(cnt > k && nxt < n) {
+			cnt -= (s[nxt] == '1');
+			nxt++;
+		}
+		swap(s[i], s[nxt]);
+		nxt++;
+		k -= cnt;
+	}
+	cout << s << nl;
+
+}	
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
+
+    return 0;
+}
