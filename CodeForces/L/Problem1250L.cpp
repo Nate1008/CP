28c15007425eae2cf471805e279df2cfced66a65
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-14 13:29:47 -0400

    Updating...

---
+++ CodeForces/L/Problem1250L.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define fi first
+#define se second
+#define vt vector
+#define vi vector<int>
+#define vl vector<long long>
+#define st set
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
+#define maxi(a, b, c) max(a, max(b, c))
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+
+	
+void solve() {
+	int a, b, c; cin >> a >> b >> c;
+	int ans = maxi(a, b, c);
+
+	int x = a, y = b, z = c;
+	while(y >= 0) {
+		int h = (y+z+1)/2;
+		ans = min(ans, maxi(x, h, h));
+		x++, y--;
+	}
+
+	x = a, y = b, z = c;
+	while(y >= 0) {
+		int h = (x+y+1)/2;
+		ans = min(ans, maxi(z, h, h));
+		z++, y--;
+	}
+
+	cout << ans << nl;
+
+}	
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
+
+    return 0;
+}
