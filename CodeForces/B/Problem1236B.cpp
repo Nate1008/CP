676b2834bbd96fc2ea37185250aa614a2efcf548
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-14 20:10:27 -0400

    Updating...

---
+++ CodeForces/B/Problem1236B.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define fi first
+#define se second
+#define vt vector
+#define vi vector<int>
+#define vl vector<long long>
+#define st set
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+const int mod = 1e9+7;
+ll fast_pow(ll a, ll p) {
+    ll res = 1;
+    while (p) {
+        if (p % 2 == 0) {
+            a = (a * a) % mod;
+            p /= 2;
+        } else {
+            res = (res * a) % mod;
+            p--;
+        }
+    }
+    return res;
+}
+
+void solve() {
+	int n, m; cin >> n >> m;
+	ll val = fast_pow(2, m)-1;
+	ll ans = fast_pow(val, n);
+	cout << ans << nl;
+}	
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    // TC
+    	solve();
+
+    return 0;
+}
--- CodeForces/B/Problem1236B.py
+++
@@ -1,2 +0,0 @@
-n, m = map(int, input().split())
-print(pow((2**m)-1, n, (10**9)+7))
