a9630f86bdecc22a69936f66c747ba52e935dcc4
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-17 07:47:30 -0400

    Updating...

---
+++ Competitions/Atcoder/ABC194/B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define fi first
+#define se second
+#define vt vector
+#define vi vector<int>
+#define vl vector<long long>
+#define st set
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+void solve() {
+	int n; cin >> n;
+	vi a(n), b(n);
+	FOR(n, i) {
+		cin >> a[i] >> b[i];
+	}
+	int ans = INT_MAX;
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			int t = 0;
+			if (i == j) {
+				t = a[i]+b[j];
+			} else {
+				t = max(a[i], b[j]);
+			}
+			ans = min(ans, t);
+		}
+	}
+	cout << ans << nl;
+}	
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    // TC
+    	solve();
+
+    return 0;
+}
---
+++ Competitions/Atcoder/ABC194/C.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define fi first
+#define se second
+#define vt vector
+#define vi vector<int>
+#define vl vector<long long>
+#define st set
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+void solve() {
+	int n; cin >> n;
+	vi cnt(420, 0);
+	vi a(n);
+	for(auto& c : a) {
+		cin >> c;
+		c += 200;
+		cnt[c]++;
+	}
+	ll ans = 0;
+	for(int i = 0; i < 421; i++) {
+		for(int j = 0; j < 421; j++) {
+			ans += (cnt[i]*1LL*cnt[j])*(abs(i-j)*abs(i-j));
+		}
+	}
+	assert(ans % 2 == 0);
+	cout << ans/2 << nl;
+
+
+}	
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    // TC
+    	solve();
+
+    return 0;
+}
