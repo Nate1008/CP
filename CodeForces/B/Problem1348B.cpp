00bbc705166664bc5b0ca84d99c3a6921de8c934
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-20 07:59:42 -0400

    Updating...

--- CodeForces/B/Problem1348B.cpp
+++ CodeForces/B/Problem1348B.cpp
@@ -2,86 +2,70 @@
 
 #define ll long long
 #define pi pair<int, int>
-#define pf pair<float, int>
-#define ps pair<string, int>
-#define pii pair<pi, int>
 #define mi map<int, int>
-#define ms map<string, int>
-#define mc map<char, int>
-#define mpi map<pi, int>
-#define F first
-#define S second
+#define fi first
+#define se second
 #define vt vector
 #define vi vector<int>
-#define vd vector<double>
-#define vf vector<float>
 #define vl vector<long long>
-#define vpi vector<pi>
-#define vpf vector<pf>
-#define vpii vector<pii>
 #define st set
-#define si set<int>
-#define sd set<double>
-#define sf set<float>
-#define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
-#define RFOR(t, q) for(int q = t; q > 0; q--)
-#define FORE(t, q) for(int q = 0; q <= t; q++)
-#define RFORE(t, q) for(int q = t; q >= 0; q--)
-
 #define FORN(t, q, v) for(int q = v; q < t; q++)
-#define RFORN(t, q, v) for(int q = t; q > v; q--)
-#define FOREN(t, q, v) for(int q = v; q <= t; q++)
-#define RFOREN(t, q, v) for(int q = t; q >= v; q--)
-
-#define FORV(v, type) for (vt<type>::iterator it = v.begin(); it <= v.end(); it++)
 #define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
 
-
-#define ALL(v) v.begin(), v.end()
-#define RALL(v) v.rbegin(), v.rend()
-
-#define B(v) v.begin()
-#define RB(v) v.rbegin()
-#define E(v) v.end()
-#define RE(v) v.rend()
-
-#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
-#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
-
-#define TC int t; cin >> t; FOR(t, q)
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
 
 using namespace std;
+
+void solve() {
+	int n, k; cin >> n >> k;
+	vi a(n), ele;
+	unordered_set<int> uni;
+	for(auto& c : a) {
+		cin >> c;
+		uni.insert(c);
+	}
+
+	if (uni.size() > k) {
+		cout << -1 << nl;
+		return;
+	}
+	for(auto& c : uni) {
+		ele.pb(c);
+	}
+
+	int nxt = 1;
+	while(nxt <= n && sz(ele) < k) {
+		if (uni.find(nxt) == uni.end()) ele.pb(nxt);
+		nxt++;
+	}
+
+
+	vi ans;
+	for(int i = 0, j = 0; i < n; j++, j %= sz(ele)) {
+		if (ele[j] == a[i]) {
+			ans.pb(a[i]);
+			i++; 
+		} else {
+			ans.pb(ele[j]);
+		}
+	}
+	cout << sz(ans) << nl;
+	for(auto c : ans) cout << c << " ";
+	cout << nl;
+
+}	
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    TC
+    	solve();
 
-    TC {
-        int N,K;
-        cin>>N>>K;
-        set<int>s;
-        for (int i=0;i<N;i++){
-            int a;
-            cin>>a;
-            s.insert(a);
-        }
-        //if more than K distinct numbers, print -1
-        if (s.size()>K){
-            cout<<-1<<endl;
-            continue;
-        }
-        cout<<N*K<<endl;
-        for (int i=0;i<N;i++){
-            //print the distinct numbers
-            for (int b: s)
-                cout<<b<<' ';
-            //print the extra 1s
-            for (int j=0;j<K-(int)s.size();j++)
-                cout<<1<<' ';
-        }
-        cout<<endl;
-    }
     return 0;
 }
