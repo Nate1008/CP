110b1f2aac63a82a190f50ed8ffc27f56971f835
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-22 08:30:11 -0400

    Updating...

---
+++ Competitions/1516/a/a.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define fi first
+#define se second
+#define vt vector
+#define vi vector<int>
+#define vl vector<long long>
+#define st set
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
+#define ppb(s) pop_back(s);
+
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+void solve() {
+	int n, k; cin >> n >> k;
+	vi v(n);
+	for(auto& c : v) cin >> c;
+
+	int a = -1;
+	for(int i = 0, b = 0; i < k; ++i) {
+		while(b < n-1 && v[b] == 0) b++;
+		if (b == n-1) break;
+		a = (n-1)-(b == n-1);
+		v[a]++, v[b]--;
+		// cout << a << " " << b << nl;
+	}
+	for(auto& c : v) cout << c << " ";
+	cout << nl;
+}	
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
+
+    return 0;
+}
---
+++ Competitions/1516/b/b.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define fi first
+#define se second
+#define vt vector
+#define vi vector<int>
+#define vl vector<long long>
+#define st set
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
+#define ppb(s) pop_back(s);
+
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+void solve() {
+	int n; cin >> n;
+	vi a(n);
+	for(auto& c : a) {
+		cin >> c;
+	}
+	
+	for(int mid = 0; mid < n; mid++) {
+		int pfx = 0, sfx = 0;
+		for(int l = 0; l < mid; l++) pfx ^= a[l];
+		for(int r = mid+1; r < n; r++) sfx ^= a[r];
+		if (mid == (pfx^sfx)) {
+			cout << "YES" << nl;
+			return;
+		}
+	}
+	cout << "NO" << nl; 
+
+}	
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
+
+    return 0;
+}
