db1594eceb95565762dad99b6444f172dba17c5a
Nate1008 <pidgeypiggy1000@gmail.com>
2021-04-23 08:25:21 -0400

    Updating...

---
+++ CodeForces/B/Problem1508B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pi pair<int, int>
+#define mi map<int, int>
+#define fi first
+#define se second
+#define vt vector
+#define vi vector<int>
+#define vl vector<long long>
+#define st set
+
+#define FOR(t, q) for(int q = 0; q < t; q++)
+#define FORN(t, q, v) for(int q = v; q < t; q++)
+#define sz(v) (int)(v.size())
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb(s) push_back(s);
+#define ppb(s) pop_back(s);
+
+
+#define nl endl
+#define TC int _t; cin >> _t; FOR(_t, _q)
+
+using namespace std;
+
+void solve() {
+	int n; ll k; cin >> n >> k;
+	k--;
+	vi f(n-1);
+	for(int i = n-2; i >= 0; i--) {
+		f[i] = k%2;
+		k /= 2;
+	}
+	if (k > 0) {
+		cout << -1 << nl;
+		return;
+	}
+	f.pb(k);
+
+	int beg = 0;
+	while(beg < n) {
+		int end = beg;
+		while(end+1 < n && f[end] == 1) end++;
+		for(int i = beg; i <= end; i++) {
+			cout << (beg+end-i)+1 << " ";
+		}
+		beg = ++end;
+	}
+	cout << nl;
+
+
+}		
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    TC
+    	solve();
+
+    return 0;
+}
