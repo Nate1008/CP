6809f4b4ebafcf22c40426ff9bfa7d0c01411303
Nate1008 <pidgeypiggy1000@gmail.com>
2021-05-15 16:57:42 -0400

    Updating...

--- CodeForces/B/Problem1282B.cpp
+++ CodeForces/B/Problem1282B.cpp
@@ -2,38 +2,22 @@
 
 #define ll long long
 #define pi pair<int, int>
-#define pf pair<float, int>
-#define ps pair<string, int>
-#define pii pair<int, pi>
 #define mi map<int, int>
-#define ml map<long long, int>
-#define ms map<string, int>
-#define mc map<char, int>
-#define mpi map<pi, int>
-#define F first
-#define S second
+#define fi first
+#define se second
 #define vt vector
 #define vi vector<int>
-#define vd vector<double>
-#define vf vector<float>
 #define vl vector<long long>
-#define vpi vector<pi>
-#define vpf vector<pf>
-#define vpii vector<pii>
 #define st set
-#define si set<int>
-#define sd set<double>
-#define sf set<float>
-#define sl set<long long>
 
 #define FOR(t, q) for(int q = 0; q < t; q++)
 #define FORN(t, q, v) for(int q = v; q < t; q++)
 #define sz(v) (int)(v.size())
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
-#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
-#define UPPER(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
 #define pb(s) push_back(s);
+#define ppb(s) pop_back(s);
+
 
 #define nl endl
 #define TC int _t; cin >> _t; FOR(_t, _q)
@@ -43,24 +27,38 @@ using namespace std;
 void solve() {
 	int n, p, k; cin >> n >> p >> k;
 	vi a(n);
-	FOR(n, i) cin >> a[i];
+	for(auto& c : a) cin >> c;
 	sort(all(a));
-	int ans = 0; ll sum = 0;
-	for(int i = 0; i < n; i+=2) {
-		sum += a[i];
-		if (sum <= p) ans = max(ans, i+1);
-	} 
-	sum = 0;
-	for(int i = 1; i < n; i+=2) {
-		sum += a[i];
-		if (sum <= p) ans = max(ans, i+1);
+	
+	vi pfx = {0};
+	for(int i = 0; i < k-1.; i++) {
+		pfx.push_back(pfx.back()+a[i]);
 	}
-	cout << ans << nl;
-}
+
+	int ans = 0, sum = 0, pos = 0;
+	for(int t = 0; t < k; t++) {
+		sum = 0, pos = t;
+		while(pos > 0 && pfx[pos] > p) pos--;
+		ans = max(pos, ans);
+		for(int i = t+k-1; i < n; i += k) {
+			sum += a[i];
+			if (sum > p) break;
+			int dif = p-sum;
+			while(pos > 0 && pfx[pos] > dif) pos--;
+			ans = max(ans, pos+i-t+1);
+		}
+	}
+	cout << ans << endl;
+}	
 
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    cin.tie(0);	
+
+    #ifdef LOCAL
+		freopen("in.in", "r", stdin);
+	#endif
+
     TC
     	solve();
 
