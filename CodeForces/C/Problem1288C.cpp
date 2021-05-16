bca097dfd5c84a10b915006e41347bf0dd903d1a
Nate1008 <pidgeypiggy1000@gmail.com>
2021-05-16 12:00:44 -0400

    Updating...

--- CodeForces/C/Problem1288C.cpp
+++ CodeForces/C/Problem1288C.cpp
@@ -24,12 +24,41 @@
 
 using namespace std;
 
+const int mod = 1e9+7;
 
+ll fast_pow(ll a, ll p) {
+    ll res = 1;
+    while (p) {
+        if (p % 2 == 0) {
+            a = (a * a) % mod;
+            p /= 2;
+        } else {
+            res = (res * a) % mod;
+            p--;
+        }
+    }
+    return res;
+}
+
+
+ll fact(int n) {
+  ll res = 1;
+  for (int i = 1; i <= n; i++) {
+    res %= mod;
+    res = res * 1LL * i % mod;
+  }
+  return res % mod;
+}
+
+
+ll C(int n, int k) {
+  return fact(n) * 1ll * fast_pow(fact(k), mod - 2) % mod * 1ll * fast_pow(fact(n - k), mod - 2) % mod;
+}
 
 void solve() {
-	int N, M; cin >> N >> M;
-
-}	
+	int n, m; cin >> n >> m;
+	cout << C(n+(2*m)-1, (2*m)) % mod << endl;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -39,7 +68,7 @@ int main() {
 		freopen("in.in", "r", stdin);
 	#endif
 
-    TC
+    // TC
     	solve();
 
     return 0;
