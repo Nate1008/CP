35d32453f60b1bf2ee3535a639ade2a28dfc3d81
Nate10 <70302601+Nate1008@users.noreply.github.com>
2021-07-13 15:47:36 -0400

    Update

--- Algos.cpp
+++ Algos.cpp
@@ -222,3 +222,31 @@ struct HashRange {
         return cum[r+1]-pows[len]*cum[l]; }
 };
 //Hash Range - End
+
+// SCC - Start
+struct SCC {
+	int N, ti = 0; vector<vector<int>> adj;
+	vector<int> disc, comp, st, comps;
+	void init(int _N) {
+		N = _N;
+		adj.resize(N), disc.resize(N), comp = vector<int>(N,-1);
+	}
+	void ae(int x, int y) {
+		adj[x].push_back(y);
+	}
+	int dfs(int x) {
+		int low = disc[x] = ++ti; st.push_back(x); // disc[y] != 0 -> in stack
+		for(int y : adj[x]) if (comp[y] == -1) low = min(low, disc[y]?:dfs(y));
+		if (low == disc[x]) { // make new SCC, pop off stack until you find x
+			comps.push_back(x); for (int y = -1; y != x;)
+				comp[y = st.back()] = x, st.pop_back();
+		}
+		return low;
+	}
+	void gen() {
+		for(int i=0; i<N; i++) if (!disc[i]) dfs(i);
+		reverse(begin(comps), end(comps));
+	}
+};
+// SCC - End
+
