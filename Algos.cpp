63dfabb1562446b9b132fc86cf99b528335cf87b
Nate10 <70302601+Nate1008@users.noreply.github.com>
2021-12-29 14:38:47 -0500

    Change BIT a bi9t

--- Algos.cpp
+++ Algos.cpp
@@ -178,19 +178,9 @@ tcT> struct LazySeg {
 tcT> struct BIT {
 	int N; V<T> data;
 	void init(int _N) { N = _N; data.rsz(N); }
-	void add(int p, T x) { for (++p;p<=n;p+=p&-p) data[p-1] += x; }
+	void add(int p, T x) { for (++p;p<=N;p+=p&-p) data[p-1] += x; }
 	T sum(int l, int r) { return sum(r+1)-sum(l); }
 	T sum(int r) { T s = 0; for(;r;r-=r&-r)s+=data[r-1]; return s; }
-	int lower_bound(T sum) {
-		if (sum <= 0) return -1;
-		int pos = 0;
-		for (int pw = 1<<25; pw; pw >>= 1) {
-			int npos = pos+pw;
-			if (npos <= n && data[npos-1] < sum)
-				pos = npos, sum -= data[pos-1];
-		}
-		return pos;
-	}
 };
 // BIT - End
 
